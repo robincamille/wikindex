dfe550485d0433c766990483bfda04e5d67b3993
Robin <rdvs18@gmail.com>
2013-05-24 12:57:19 -0400

    cleaned up

--- index to wiki-ready list/index-to-wiki.py
+++ index to wiki-ready list/index-to-wiki.py
@@ -8,12 +8,12 @@ filename=open('xxxxx.txt','r') #copy and paste the index into a file, replace xx
 fcontent=filename.read()
 filename.close()
 
-fcontent1 = re.sub(r'(\d{0,2})','',fcontent) #take out page numbers (does not take out Roman numerals)
-fcontent2 = re.sub('("|\s\(.*?\))','',fcontent1) #take out double quotation marks and anything in parens
-fcontent3 = re.sub(',,',',',fcontent2) #take out double commas
-fcontent4 = re.sub(r'((,|\s|\.)+\n)','\n',fcontent3) #take out endline commas, spaces, and periods
+fcontent = re.sub(r'(\d{0,2})','',fcontent) #take out page numbers (does not take out Roman numerals)
+fcontent = re.sub(r'"','',fcontent) #take out double quotation marks
+fcontent = re.sub(r',,',',',fcontent) #take out double commas
+fcontent = re.sub(r'((,|\s|\.)+\n)','\n',fcontent) #take out endline commas, spaces, and periods
 
-lines = re.split("\n+", fcontent4)
+lines = re.split("\n+", fcontent)
 
 for line in lines:
      line = re.split(", ",line)
@@ -27,7 +27,7 @@ for line in lines:
 
      if re.match(r"^\*\[\[(([A-Z](\.|\s|\]))|Dr\.|Mrs(\.*)\s|Rev\.|Miss\s|Prof\.|Mr(\.*)\s|Messrs(\.*))",line): #ignore names beginning with initials, like J. Adams, or titles
           pass
-     elif re.search(r"(See|see|Index|INDEX)",line): #remove lines that include 'see' or 'index'
+     elif re.search(r"(See|see|Index|INDEX|\(|\)|\~)",line): #remove lines that include 'see' or 'index' or parens or tilde
           pass                   
      elif re.match(r"^\*\[\[[A-Z]",line): #only names beginning with capital letters
           outText.write(line)
--- split list into columns/readme.txt
+++ split list into columns/readme.txt
@@ -1,4 +1,4 @@
-Breaks list into columms of 30 items and outputs index-listified.txt
+Breaks list into columns of X items and outputs index-listified.txt
 
 For use by default with index-wikified.txt (after you've weeded out bad entries)
 but can be used with any list! 
